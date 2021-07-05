f55b61117e79183b4c50aa53c1ceeeddb9dc56ec
Nikita <kononenko.n@phystech.edu>
2021-07-05 10:37:15 +0300

    added hw11

--- demo/src/main/java/com/example/mservicekafka/kafka/Topics.java
+++
@@ -1,8 +0,0 @@
-package com.example.mservicekafka.kafka;
-
-public class Topics {
-
-    // Через консоль Apache Kafka отправить сообщение в топик "demo"
-    public static final String DEMO = "demo";
-
-}
